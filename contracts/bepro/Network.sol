12a74289209f87c355deaacbd904e42db012e7f0
Ruiub3i <ruivaz1997@gmail.com>
2021-04-30 11:38:59 +0100

    Network first version finalized

--- contracts/bepro/Network.sol
+++ contracts/bepro/Network.sol
@@ -42,6 +42,10 @@ contract BEPRONetwork is Pausable, Ownable{
     uint256 public percentageNeededForMerge = 20; // (%) - Amount needed to approve a PR and distribute the rewards
     uint256 public beproVotesStaked = 0;
 
+    uint256 public COUNCIL_BEPRO_AMOUNT = 25000000; // 25M
+    uint256 public OPERATOR_BEPRO_AMOUNT = 1000000; // 10M
+    uint256 public DEVELOPER_BEPRO_AMOUNT = 10000; // 10k
+
     mapping(uint256 => Issue) public issues; /* Distribution object */
     mapping(address => uint256[]) public myIssues; /* Address Based Subcription */
 
@@ -80,7 +84,7 @@ contract BEPRONetwork is Pausable, Ownable{
     event OpenIssue(uint256 indexed id, address indexed opener, uint256 indexed amount);
     event ApproveIssue(uint256 indexed id, uint256 indexed votes, address indexed approver);
     event MergeProposalCreated(uint256 indexed id, uint256 indexed mergeID, address indexed creator);
-    event ApproveMerge(uint256 indexed id, uint256 indexed mergeID, uint256 indexed votes, address indexed approver);
+    event ApproveMerge(uint256 indexed id, uint256 indexed mergeID, uint256 votes, address indexed approver);
     event CloseIssue(uint256 indexed id, uint256 indexed mergeID, address[] indexed addresses);
 
     constructor(address _tokenAddress) public { 
@@ -88,51 +92,52 @@ contract BEPRONetwork is Pausable, Ownable{
     }
 
     function lockVotes(uint256 _beproAmount) public {
+        require(_beproAmount > 0, "BEPRO Amount is to be higher than 0");
         require(beproToken.transferFrom(msg.sender, address(this), _beproAmount), "Needs Allowance");
 
-        if(voters[msg.sender] != 0){
+        if(voters[msg.sender].beproLocked != 0){
             // Exists
-            voters[msg.sender].votesDelegated[msg.sender] = voter[msg.sender].votesDelegated[msg.sender].add(_beproAmount);
+            voters[msg.sender].votesDelegated[msg.sender] = voters[msg.sender].votesDelegated[msg.sender].add(_beproAmount);
             voters[msg.sender].beproLocked = voters[msg.sender].beproLocked.add(_beproAmount);
         }else{
             // Does not exist
-            Voter memory voter;
+            Voter storage voter = voters[msg.sender];
             voter.beproLocked = _beproAmount;
-            voter.delegatedVotesAddresses.push(msg.sender);
+            voter.delegatedVotesAddresses = [msg.sender];
             voter.votesDelegated[msg.sender] = _beproAmount;
             votersArray.push(msg.sender);
         }
     }
 
     function unlockVotes(uint256 _beproAmount, address _from) public {
-        Voter memory voter = voters[msg.sender];
-        require(voter.beproLocked >= _beproVotes, "Has to have bepro to unlock");
-        require(voter.votesDelegated[_from] >= _beproVotes, "From has to have bepro to unlock");
+        Voter storage voter = voters[msg.sender];
+        require(voter.beproLocked >= _beproAmount, "Has to have bepro to unlock");
+        require(voter.votesDelegated[_from] >= _beproAmount, "From has to have bepro to unlock");
 
-        voters[msg.sender].beproLocked = voter.beproLocked.sub(_beproVotes);
-        voters[msg.sender].votesDelegated[_from] = voter.votesDelegated[_from].sub(_beproVotes);
+        voters[msg.sender].beproLocked = voter.beproLocked.sub(_beproAmount);
+        voters[msg.sender].votesDelegated[_from] = voter.votesDelegated[_from].sub(_beproAmount);
         if(msg.sender != _from){
-            voters[_from].votesDelegatedByOthers = voters[_from].votesDelegatedByOthers.sub(_beproVotes);
+            voters[_from].votesDelegatedByOthers = voters[_from].votesDelegatedByOthers.sub(_beproAmount);
         }
 
         require(beproToken.transfer(msg.sender, _beproAmount), "Transfer didnt work");
-        beproVotesStaked.sub(_beproVotes);
+        beproVotesStaked.sub(_beproAmount);
     }
 
-    function delegateVotes(uint256 _beproVotes, address _delegatedTo) internal {
-        Voter memory voter = voters[msg.sender];
+    function delegateVotes(uint256 _beproAmount, address _delegatedTo) internal {
+        Voter storage voter = voters[msg.sender];
 
         require(_delegatedTo != address(0), "Cannot transfer to the zero address");
         require(_delegatedTo != msg.sender, "Cannot transfer to itself");
 
-        require(voter.beproLocked >= _beproVotes, "Has to have bepro to unlock");
-        require(voter.votesDelegated[msg.sender] >= _beproVotes, "From has to have bepro to unlock");
+        require(voter.beproLocked >= _beproAmount, "Has to have bepro to unlock");
+        require(voter.votesDelegated[msg.sender] >= _beproAmount, "From has to have bepro to unlock");
 
-        votes[msg.sender].votesDelegated[msg.sender] = voter.votesDelegated[msg.sender].sub(_beproVotes);
-        votes[msg.sender].votesDelegated[_delegatedTo] = voter.votesDelegated[_delegatedTo].add(_beproVotes);
+        voters[msg.sender].votesDelegated[msg.sender] = voter.votesDelegated[msg.sender].sub(_beproAmount);
+        voters[msg.sender].votesDelegated[_delegatedTo] = voter.votesDelegated[_delegatedTo].add(_beproAmount);
 
-        require(votes[_delegatedTo].beproLocked != address(0), "Delegated to has to have voted already");
-        voters[_delegatedTo].votesDelegatedByOthers = voters[_delegatedTo].votesDelegatedByOthers.add(_beproVotes);
+        require(voters[_delegatedTo].beproLocked != uint256(0), "Delegated to has to have voted already");
+        voters[_delegatedTo].votesDelegatedByOthers = voters[_delegatedTo].votesDelegatedByOthers.add(_beproAmount);
     }
 
     function approveIssue(uint256 _issueID) public {
@@ -151,7 +156,7 @@ contract BEPRONetwork is Pausable, Ownable{
     function approveMerge(uint256 _issueID, uint256 _mergeID) public {
         Voter memory voter = voters[msg.sender];
         Issue memory issue = issues[_issueID];
-        MergeProposal memory merge = issues[_issueID].mergeProposals[_mergeID];
+        MergeProposal storage merge = issues[_issueID].mergeProposals[_mergeID];
         require(issue._id != 0, "Issue does not exist");
         require(issue.mergeIDIncrement >  _mergeID, "Merge Proposal does not exist");
         require(merge.votesForMergeByAddress[msg.sender] == 0, "Has already voted");
@@ -172,8 +177,14 @@ contract BEPRONetwork is Pausable, Ownable{
         return (issues[_issueID].mergeProposals[_mergeID].votes >= beproVotesStaked.mul(percentageNeededForMerge).div(100));
     }
     
-    function isMergeTheOneWithMoreVotes(uint256 _issueID, uint256 _mergeID) public returns (boo) {
-        return (issues[_issueID].mergeProposals[_mergeID].votes >= beproVotesStaked.mul(percentageNeededForMerge).div(100));
+    function isMergeTheOneWithMoreVotes(uint256 _issueID, uint256 _mergeID) public returns (bool) {
+        uint256 thisMergeVotes = issues[_issueID].mergeProposals[_mergeID].votes;
+        for(uint8 i = 0; i < issues[_issueID].mergeIDIncrement; i++){
+            if(issues[_issueID].mergeProposals[i].votes > thisMergeVotes){
+                return false;
+            }
+        }
+        return true;
     }
 
     /**
@@ -187,7 +198,6 @@ contract BEPRONetwork is Pausable, Ownable{
         issue.beproStaked = _beproAmount;
         issue.issueGenerator = msg.sender;
         issue.finalized = false;
-        issue.approved = false;
         issues[incrementIssueID] = issue;
         myIssues[msg.sender].push(incrementIssueID);
         // Stake bepro
@@ -220,13 +230,19 @@ contract BEPRONetwork is Pausable, Ownable{
         }
     }
 
+  /**
+     * @dev Owner finalizes the issue and distributes the bepro or rejects the PR
+     * @param _issueID issue id (mapping with github)
+     * @param _prAddresses PR Address
+     * @param _prAmounts PR Amounts
+     */
     function proposeIssueMerge(uint256 _issueID, address[] memory _prAddresses, uint256[] memory _prAmounts) public whenNotPaused {
         
         Issue memory issue = issues[_issueID];
         require(issue._id != 0 , "Issue has to exist");
         require(issue.finalized == false, "Issue has to be opened");
         require(_prAmounts.length == _prAddresses.length, "Amounts has to equal addresses length");
-        require(/* BEPRO Amount above 1000 */);
+        require(beproToken.balanceOf(msg.sender) > COUNCIL_BEPRO_AMOUNT*10**18, "To propose merges the proposer has to be a Council (COUNCIL_BEPRO_AMOUNT)");
 
         MergeProposal memory mergeProposal;
         mergeProposal._id = issue.mergeIDIncrement;
@@ -237,6 +253,7 @@ contract BEPRONetwork is Pausable, Ownable{
         uint256 total = ((issues[_issueID].beproStaked * (feeShare + mergeCreatorFeeShare)) / 100); // Fee + Merge Creator Fee + 0
 
         for(uint i = 0; i < _prAddresses.length; i++){
+            require(beproToken.balanceOf(_prAddresses[i]) > DEVELOPER_BEPRO_AMOUNT*10**18, "To receive development rewards the rewarded has to be a Developer (DEVELOPER_BEPRO_AMOUNT)");
             total = total.add((_prAmounts[i] * (100-feeShare-mergeCreatorFeeShare)) / 100);
         }
 
@@ -244,7 +261,7 @@ contract BEPRONetwork is Pausable, Ownable{
 
         issues[_issueID].mergeProposals[issue.mergeIDIncrement] = mergeProposal;
         issues[_issueID].mergeIDIncrement = issues[_issueID].mergeIDIncrement + 1;
-        emit MergeProposalCreated(_issueID, mergeID, msg.sender);
+        emit MergeProposalCreated(_issueID, mergeProposal._id, msg.sender);
     }
 
 
@@ -252,8 +269,7 @@ contract BEPRONetwork is Pausable, Ownable{
     /**
      * @dev Owner finalizes the issue and distributes the bepro or rejects the PR
      * @param _issueID issue id (mapping with github)
-     * @param _prAddresses PR Address
-     * @param _prAmounts PR Amounts
+     * @param _mergeID merge id 
      */
     function closeIssue(uint256 _issueID, uint256 _mergeID) public whenNotPaused {
         Issue memory issue = issues[_issueID];
@@ -274,14 +290,14 @@ contract BEPRONetwork is Pausable, Ownable{
         require(beproToken.transfer(feeAddress, (issues[_issueID].beproStaked * mergeCreatorFeeShare) / 100), "Has to transfer");
         
         // Generate Reputation Tokens
-        for(uint i = 0; i < merge._prAddresses.length; i++){
-            myIssues[merge._prAddresses[i]].push(_issueID);
-            require(beproToken.transfer(merge._prAddresses[i], (merge._prAmounts[i] * (100-feeShare-mergeCreatorFeeShare)) / 100), "Has to transfer");
+        for(uint i = 0; i < merge.prAddresses.length; i++){
+            myIssues[merge.prAddresses[i]].push(_issueID);
+            require(beproToken.transfer(merge.prAddresses[i], (merge.prAmounts[i] * (100-feeShare-mergeCreatorFeeShare)) / 100), "Has to transfer");
         }
 
         closedIdsCount = closedIdsCount.add(1);
 
-        emit CloseIssue(_mergeID, mergeId, merge._prAddresses);
+        emit CloseIssue(_issueID, _mergeID, merge.prAddresses);
     }
 
     function getIssuesByAddress(address _address) public returns (uint256[] memory){
@@ -289,7 +305,7 @@ contract BEPRONetwork is Pausable, Ownable{
     }
 
     function getVotesByAddress(address _address) public returns (uint256){
-        Voter memory voter = voters[_address];
+        Voter storage voter = voters[_address];
         return voter.votesDelegatedByOthers.add(voter.votesDelegated[_address]);
     }
     
@@ -299,7 +315,7 @@ contract BEPRONetwork is Pausable, Ownable{
     }
 
     function getMergeById(uint256 _issueID, uint256 _mergeId) public returns (uint256, uint256, address[] memory, uint256[] memory, address){
-        MergeProposal memory merge = issues[_issueID].mergeProposals[_mergeID];
+        MergeProposal memory merge = issues[_issueID].mergeProposals[_mergeId];
         return (merge._id, merge.votes, merge.prAddresses, merge.prAmounts, merge.proposalAddress);
     }
 
