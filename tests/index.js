14b5b470927a788d756c3dd53550b6351a659010
Ruiub3i <ruivaz1997@gmail.com>
2021-06-01 17:02:04 +0100

    remove duplicate

--- tests/index.js
+++ tests/index.js
@@ -2,7 +2,6 @@ context("Unit Tests", async () => {
   require("./application");
   require("./generics");
   require("./bepro/network");
-  require("./bepro/network");
   require("./erc20Contract");
   require('./dexStorage');
   require("./erc20TokenLock");
