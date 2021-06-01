20e521ca4fafd040d391ce4aa43d19d1dddef96f
Ruiub3i <ruivaz1997@gmail.com>
2021-06-01 19:49:48 +0100

    network update

--- src/models/BEPRO/Network.js
+++ src/models/BEPRO/Network.js
@@ -7,7 +7,7 @@ import ERC20Contract from '../ERC20/ERC20Contract';
 
 
 /**
- * @typedef {Object} StakingContract~Options
+ * @typedef {Object} Network~Options
  * @property {Boolean} test
  * @property {Boolean} localtest ganache local blockchain
  * @property {Web3Connection} [web3Connection=Web3Connection] created from params: 'test', 'localtest' and optional 'web3Connection' string and 'privateKey'
