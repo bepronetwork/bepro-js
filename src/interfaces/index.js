333de70b75a8a5300ae00c1a41b7999051c92eea
Ricardo Marques <ricardo.ds.marques@gmail.com>
2021-06-29 17:35:15 +0100

    add realitio to interfaces

--- src/interfaces/index.js
+++ src/interfaces/index.js
@@ -1,8 +1,8 @@
 let index = {
-	exchange	: require("../../build/contracts/Exchange.json"),
-	staking 	: require("../../build/contracts/StakingContract.json"),
+	staking		: require("../../build/contracts/StakingContract.json"),
 	prediction: require("../../build/contracts/PredictionMarket.json"),
 	ierc20 		: require("../../build/contracts/Token.json"),
+	realitio	: require("../../build/contracts/RealitioERC20.json"),
 };
 
 module.exports = index;
