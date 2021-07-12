7e2025351384e4135fa081a6c9789c855546e8f2
Ricardo Marques <ricardo.ds.marques@gmail.com>
2021-07-12 17:52:54 +0100

    fix bepro interface

--- src/interfaces/index.js
+++ src/interfaces/index.js
@@ -1,6 +1,6 @@
 let index = {
 	prediction: require("../../build/contracts/PredictionMarket.json"),
-	ierc20 		: require("../../build/contracts/Token.json"),
+	ierc20 		: require("../../build/contracts/ERC20.json"),
 	realitio	: require("../../build/contracts/RealitioERC20.json"),
 };
 
