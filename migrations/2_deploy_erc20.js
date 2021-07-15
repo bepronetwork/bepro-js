3b9273fe09ecac2bd81c0dc0fc9607707afacde1
Ricardo Marques <ricardo.ds.marques@gmail.com>
2021-07-15 12:48:40 +0100

    fix erc20 migration

--- migrations/2_deploy_erc20.js
+++ migrations/2_deploy_erc20.js
@@ -3,6 +3,7 @@ const ERC20PresetMinterPauser = artifacts.require("ERC20PresetMinterPauser");
 
 module.exports = async function(deployer, network, accounts) {
   // NOTE: USE THIS ONLY FOR DEV PURPOSES!!
-  const token = await deployer.deploy(ERC20PresetMinterPauser, "Polkamarkets", "POLK");
+  await deployer.deploy(ERC20PresetMinterPauser, "Polkamarkets", "POLK");
+  const token = await ERC20PresetMinterPauser.deployed();
   await token.mint('0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1', '100000000000000000000000000');
 }
