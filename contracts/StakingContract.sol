65bca2e36405c16497ab3faa8bc520f113558ed0
Ruiub3i <ruivaz1997@gmail.com>
2021-09-14 16:18:14 +0100

    test fix 3

--- contracts/StakingContract.sol
+++ contracts/StakingContract.sol
@@ -86,11 +86,11 @@ contract StakingContract is Pausable, Ownable {
         
         uint256 futureAPRAmount = getAPRAmount(products[_product_id].APR, time, products[_product_id].endDate, _amount);
 
-        require( 1 < 0, "Testing");
 
         /* Confirm the current funds can assure the user the APR is valid */
         require(availableTokens() >= futureAPRAmount);
 
+        require( 1 < 0, "Testing 1");
         /* Confirm the user has funds for the transfer */
         require(erc20.transferFrom(msg.sender, address(this), _amount));
 
