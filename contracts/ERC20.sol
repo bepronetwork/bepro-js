6c160e4b18fab6f0c6516c7325cc4e2f6c65a346
Ricardo Marques <ricardo.ds.marques@gmail.com>
2021-09-20 14:42:07 +0200

    update build/contracts

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -1,11 +1,3 @@
-/**
- *Submitted for verification at Etherscan.io on 2020-09-28
-*/
-
-/**
- *Submitted for verification at Etherscan.io on 2018-09-01
-*/
-
 pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol";
