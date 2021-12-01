3ccebe15876f79269ea196eda79bc967063a5a22
SamG <cash.ws@gmail.com>
2021-12-01 21:03:20 +0000

    [FIX] -fixed test index.js file, added all test cases.

--- tests/index.js
+++ tests/index.js
@@ -1,13 +1,12 @@
 context("Unit Tests", async () => {
-  //require("./application");
-  //require("./generics");
-  //require("./votingContract");
-  //require("./bepro/network");
+  require("./application");
+  require("./generics");
+  require("./votingContract");
+  require("./bepro/network");
   require("./bepro/networkFactory");
-  //require("./erc20Contract");
-  //require('./dexStorage');
-  //require("./erc20TokenLock");
-  //require("./stakingContract");
-  //require("./sablier/sablier");
-  //require("./custom/loophole/loophole");
+  require("./erc20Contract");
+  require('./dexStorage');
+  require("./erc20TokenLock");
+  require("./stakingContract");
+  require("./custom/loophole/loophole");
 });
