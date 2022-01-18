2fac18f7ed2ab05b8966831feaf2883fac6decb3
moshmage <moshmage@gmail.com>
2022-01-18 12:12:52 +0000

    better post-install

--- tooling/post-install.js
+++ tooling/post-install.js
@@ -1,18 +1,25 @@
 const fs = require('fs');
-const {exec} = require("child_process");
 
-if (fs.existsSync('build')) {
+if (fs.existsSync('dist')) {
   console.log(`bepro-js sdk was already built.`)
-  return;
+  return 0;
 }
 
-(async function buildSolution() {
+async function buildSolution() {
   try {
-    await exec(`npm install .`);
-    await exec(`npm run build`);
+    console.log(`Building bepro-js sdk`);
+    const {execSync} = require("child_process");
+
+    if (!fs.existsSync('node_modules'))
+      await execSync(`npm install .`, {stdio: 'inherit'});
+
+    await execSync(`npm run build`, {stdio: 'inherit'});
+
     console.log(`Built bepro-js sdk`);
   } catch (e) {
     console.log(e);
     console.log(`\nFailed to build bepro-js sdk, please issue: npm explore bepro-js -- npm run build`);
   }
-})();
+}
+
+(async () => await buildSolution())();
