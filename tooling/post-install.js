ec8c93b9060abb1028cc88ae6a610a1e2592e6ff
moshmage <moshmage@gmail.com>
2022-01-20 11:51:47 +0000

    return 1 if post-install fails

--- tooling/post-install.js
+++ tooling/post-install.js
@@ -16,9 +16,11 @@ async function buildSolution() {
     await execSync(`npm run build`, {stdio: 'inherit'});
 
     console.log(`Built bepro-js sdk`);
+    return 0;
   } catch (e) {
     console.log(e);
     console.log(`\nFailed to build bepro-js sdk, please issue: npm explore bepro-js -- npm run build`);
+    return 1;
   }
 }
 
