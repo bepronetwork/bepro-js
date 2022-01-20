34b4f851eced440f698efd85c139df40d15b038b
moshmage <moshmage@gmail.com>
2022-01-20 12:52:41 +0000

    no need for such logic because cwd is already the pkg

--- tooling/post-install.js
+++ tooling/post-install.js
@@ -11,19 +11,12 @@ async function buildSolution() {
     console.log(`Building bepro-js sdk`);
     const {execSync} = require("child_process");
 
-    const pkg = JSON.parse(fs.readFileSync(`package.json`, 'utf8'));
-    const isbepro = pkg.name === `bepro-js`;
+    const cwd = path.resolve();
 
-    console.log(`cwd`, path.resolve());
+    if (!fs.existsSync(path.resolve(cwd, `node_modules`)))
+      await execSync(`npm install .`, {stdio: 'inherit', cwd});
 
-    const node_modules = fs.existsSync('node_modules');
-    const bepro_cwd = isbepro && path.resolve() || path.resolve(`node_modules`, `bepro-js`, `node_modules`);
-    const bepro_node_modules = fs.existsSync(isbepro && `node_modules` || bepro_cwd);
-
-    if (!node_modules || !isbepro && node_modules && !bepro_node_modules)
-      await execSync(`npm install .`, {stdio: 'inherit', cwd: bepro_cwd});
-
-    await execSync(`npm run build`, {stdio: 'inherit', ... !isbepro && {cwd: bepro_cwd} || {}});
+    await execSync(`npm run build`, {stdio: 'inherit', cwd});
 
     console.log(`Built bepro-js sdk`);
     return 0;
