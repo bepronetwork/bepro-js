c1af35018ee24394229320032da7e1e2866f15d1
Ricardo Marques <ricardo.ds.marques@gmail.com>
2022-01-31 16:12:37 +0800

    update truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
-var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = process.env.TRUFFLE_MNEMONIC;
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const mnemonic = process.env.TRUFFLE_MNEMONIC;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
   networks: {
@@ -9,23 +10,33 @@ module.exports = {
       port: 8545,
       network_id: "*" // Match any network id
     },
+    live: {
+      provider: new HDWalletProvider(mnemonic, "https://mainnet.infura.io/v3/37ec248f2a244e3ab9c265d0919a6cbc"),
+      network_id: 1,
+      gasPrice: 100000000000,
+      websockets: true
+    },
     moonriver: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "https://rpc.moonriver.moonbeam.network");
-      },
+      provider: new HDWalletProvider(mnemonic, "https://rpc.moonriver.moonbeam.network"),
       network_id: 1285
     },
-    moonalpha: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "https://rpc.testnet.moonbeam.network");
-      },
-      network_id: 1287
+    moonbeam: {
+      provider: new HDWalletProvider(mnemonic, "https://rpc.api.moonbeam.network"),
+      network_id: 1284
     },
     kovan: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "https://kovan.infura.io/v3/37ec248f2a244e3ab9c265d0919a6cbc");
-      },
+      provider: new HDWalletProvider(mnemonic, "https://kovan.infura.io/v3/37ec248f2a244e3ab9c265d0919a6cbc"),
       network_id: 42
+    },
+    rinkeby: {
+      provider: new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/37ec248f2a244e3ab9c265d0919a6cbc"),
+      network_id: 4,
+      gas: 5000000
+    },
+    leprichain: {
+      provider: new HDWalletProvider(mnemonic, "https://node.leprichain.blockwell.ai"),
+      network_id: 49777,
+      gasPrice: 0
     }
   },
   // Configure your compilers
@@ -49,6 +60,10 @@ module.exports = {
     }
   },
   plugins: [
-    'truffle-contract-size'
-  ]
+    'truffle-contract-size',
+    'truffle-plugin-verify'
+  ],
+  api_keys: {
+    etherscan: ETHERSCAN_API_KEY
+  }
 };
