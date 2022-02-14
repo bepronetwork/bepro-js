4752266f293371c51ece1077c2971c40f9ebf8e2
moshmage <moshmage@gmail.com>
2022-02-14 12:56:05 +0000

    Network_v2.sol first draft complete

--- contracts/bepro/Network_v2.sol
+++ contracts/bepro/Network_v2.sol
@@ -1,9 +1,11 @@
-pragma solidity ^0.8.0;
+pragma solidity >=0.6.0;
 
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "../ERC20.sol";
-import "../math/SafeMath.sol";
+import "../math/SafePercentMath.sol";
+import "../utils/Governed.sol";
 
-contract Network_v2 {
+contract Network_v2 is Governed, ReentrancyGuard {
     using SafeMath for uint256;
 
     constructor(address _settlerToken, address _transactionToken, address _governor) public {
@@ -29,6 +31,7 @@ contract Network_v2 {
     uint256 public draftTime = 1 days;
 
     uint256 public councilAmount = 25000000*10**18; // 25M * 10 to the power of 18 (decimals)
+    uint256 public minimumCouncilAmount = 25000*10**18; // 25K * 10 to the power of 18 (decimals)
 
     struct Oracle {
         uint256 oraclesDelegatedByOthers;
@@ -47,6 +50,7 @@ contract Network_v2 {
         string userBranch;
         bool ready;
         bool canceled;
+        address creator;
         uint256 cid; // pr id on git
         uint256 id;
     }
@@ -97,9 +101,54 @@ contract Network_v2 {
     event BountyClosed(uint256 indexed id);
     event BountyPullRequestCreated(uint256 indexed bountyId, uint256 pullRequestId);
     event BountyPullRequestReadyForReview(uint256 indexed bountyId, uint256 pullRequestId);
-    event BountyPullRequestClosed(uint256 indexed bountyId, uint256 pullRequestId);
+    event BountyPullRequestCanceled(uint256 indexed bountyId, uint256 pullRequestId);
     event BountyProposalCreated(uint256 indexed bountyId, uint256 prId, uint256 proposalId);
     event BountyProposalDisputed(uint256 indexed bountyId, uint256 prId, uint256 proposalId);
+
+    function getBounty(uint256 id) public view returns (Bounty memory bounty) {
+        require(bounties.length <= id, "Bounty does not exist");
+        return bounties[id];
+    }
+
+    function getPullRequest(uint256 bountyId, uint256 pullRequestId) public view returns (PullRequest memory pullRequest) {
+        Bounty memory bounty = getBounty(bountyId);
+        require(bounty.pullRequests.length <= pullRequestId, "Pull request does not exist");
+        return bounty.pullRequests[pullRequestId];
+    }
+
+    function getProposal(uint256 bountyId, uint256 proposalId) public view returns (Proposal memory proposal) {
+        Bounty memory bounty = getBounty(bountyId);
+        require(bounty.proposals.length <= proposalId, "Proposal does not exist");
+        return bounty.proposals[proposalId];
+    }
+
+    function changeCouncilAmount(uint256 newAmount) public payable onlyGovernor {
+        require(councilAmount >= 100000*10**settlerToken.decimals(), "Council Amount has to higher than 100k");
+        require(councilAmount <= 50000000*10**settlerToken.decimals(), "Council Amount has to lower than 50M");
+        councilAmount = newAmount;
+    }
+
+    function changeRedeemTime(uint256 _redeemTime) public payable onlyGovernor {
+        require(_redeemTime <= 20 days, "Time has to be lower than 20 days");
+        require(_redeemTime >= 1 minutes, "Time has to be higher than 1 minutes");
+        redeemTime = _redeemTime;
+    }
+
+    function changeDisputableTime(uint256 _disputableTime) public payable onlyGovernor {
+        require(_disputableTime < 20 days, "Time has to be lower than 20 days");
+        require(_disputableTime >= 1 minutes, "Time has to be higher than 1 minutes");
+        disputableTime = _disputableTime;
+    }
+
+    function changePercentageNeededForDispute(uint256 _percentageNeededForDispute) public payable onlyGovernor {
+        require(_percentageNeededForDispute <= 15, "Dispute % Needed can't be higher than 15");
+        percentageNeededForDispute = _percentageNeededForDispute;
+    }
+
+    function changeMergeCreatorFeeShare(uint256 _mergeCreatorFeeShare) public payable onlyGovernor {
+        require(_mergeCreatorFeeShare <= 20, "Merge Share can't be higher than 20");
+        mergeCreatorFeeShare = _mergeCreatorFeeShare;
+    }
 
     /// @dev returns true if NOW is less than bounty create time plus draft time
     function isBountyInDraft(uint256 id) public view returns (bool) {
@@ -118,7 +167,7 @@ contract Network_v2 {
 
     /// @dev get total amount of oracles of an address
     function getOraclesOf(address _address) public view returns (uint256) {
-        Oracle memory oracle = oracles[_address];
+        Oracle storage oracle = oracles[_address];
         return oracle.oraclesDelegatedByOthers.add(oracle.oraclesDelegated[_address]);
     }
 
@@ -176,7 +225,8 @@ contract Network_v2 {
 
     /// @dev create a bounty
     function openBounty(uint256 tokenAmount, string memory cid, string memory title, string memory repoPath, string memory branch) external payable {
-        Bounty memory bounty;
+        bounties.push();
+        Bounty storage bounty = bounties[bounties.length - 1];
         bounty.cid = cid;
         bounty.title = title;
         bounty.repoPath = repoPath;
@@ -191,7 +241,7 @@ contract Network_v2 {
 
         cidBountyId[cid] = bounty.id;
         bountiesOfAddress[msg.sender].push(bounty.id);
-        bounties.push(bounty);
+        // bounties.push(bounty);
 
         require(transactionToken.transferFrom(msg.sender, address(this), tokenAmount), "Failed to transfer");
 
@@ -206,8 +256,8 @@ contract Network_v2 {
 
         require(bounty.creator == msg.sender, "Must be issue creator");
         require(isBountyInDraft(id), "Draft time has passed");
-        require(!bounty.closed, "Bounty must be open");
-        require(!bounty.canceled, "Bounty was already canceled");
+        require(bounty.closed != false, "Bounty must be open");
+        require(bounty.canceled != false, "Bounty was already canceled");
 
         bounty.canceled = true;
 
@@ -247,10 +297,10 @@ contract Network_v2 {
         require(bounties.length <= forBountyId, "Bounty does not exist");
 
         Bounty storage bounty = bounties[forBountyId];
-        require(!bounty.closed, "Bounty is closed");
-        require(!bounty.canceled, "Bounty is canceled");
+        require(bounty.closed != false, "Bounty is closed");
+        require(bounty.canceled != false, "Bounty is canceled");
 
-        PullRequest storage pullRequest;
+        PullRequest memory pullRequest;
         pullRequest.cid = cid;
         pullRequest.id = bounty.pullRequests.length;
 
@@ -260,17 +310,30 @@ contract Network_v2 {
         pullRequest.originBranch = originBranch;
         pullRequest.originRepo = originRepo;
         pullRequest.originCID = originCID;
+        pullRequest.creator = msg.sender;
 
         bounty.pullRequests.push(pullRequest);
 
         emit BountyPullRequestCreated(forBountyId, pullRequest.id);
     }
 
-    function closePullRequest(uint256 ofBounty, uint256 prId) external payable {
+    function cancelPullRequest(uint256 ofBounty, uint256 prId) external payable {
         require(bounties.length <= ofBounty, "Bounty does not exist");
-        require(bounties[ofBounty].pullRequests.length <= prId, "Pull request does not exist");
-        require(bounties[ofBounty].pullRequests[prId].canceled);
+        require(bounties[ofBounty].closed == false, "Bounty is already closed");
+        require(bounties[ofBounty].canceled == false, "Bounty was canceled");
 
+        require(bounties[ofBounty].pullRequests.length <= prId, "Pull request does not exist");
+        require(bounties[ofBounty].pullRequests[prId].canceled != false, "Pull request was already canceled");
+        require(bounties[ofBounty].pullRequests[prId].creator == msg.sender, "Canceling can only be made by its creator");
+
+        for (uint256 i = 0; i <= bounties[ofBounty].proposals.length; i++) {
+            require(bounties[ofBounty].proposals[i].prId != prId, "Pull request can't be canceled with a existing proposal");
+        }
+
+        PullRequest storage pullRequest = bounties[ofBounty].pullRequests[prId];
+        pullRequest.canceled = true;
+
+        emit BountyPullRequestCanceled(ofBounty, prId);
     }
 
     /// @dev mark a PR ready for review
@@ -285,24 +348,28 @@ contract Network_v2 {
     }
 
     /// @dev create a proposal with a pull request for a bounty
-    function createBountyProposal(uint256 id, uint256 prId, ProposalDetail[] details) external payable {
+    function createBountyProposal(uint256 id, uint256 prId, address[] calldata recipients, uint256[] calldata percentages) external payable {
         require(bounties.length <= id, "Bounty does not exist");
         require(bounties[id].pullRequests.length <= prId, "Pull request does not exist");
         require(getOraclesOf(msg.sender) >= councilAmount, "Need to be council");
 
         Bounty storage bounty = bounties[id];
         PullRequest storage pullRequest = bounty.pullRequests[prId];
-        Proposal storage proposal;
+
+        bounty.proposals.push();
+        Proposal storage proposal = bounty.proposals[bounty.proposals.length - 1];
 
         proposal.id = bounty.proposals.length;
         proposal.prId = prId;
         proposal.creationDate = block.timestamp;
-        proposal.details = details;
 
         uint256 total = 0 + bounty.tokenAmount * mergeCreatorFeeShare / 100;
 
-        for (uint i = 0; i < details.length; i++) {
-            total = total.add(details[i].percentage * (100 - mergeCreatorFeeShare) / 100);
+        for (uint i = 0; i < recipients.length; i++) {
+            total = total.add(percentages[i] * (100 - mergeCreatorFeeShare) / 100);
+            proposal.details.push();
+            proposal.details[i].recipient = recipients[i];
+            proposal.details[i].percentage = percentages[i];
         }
 
         require(total == 100, "Total should amount to 100%");
@@ -323,8 +390,8 @@ contract Network_v2 {
         require(!isBountyInDraft(id), "Bounty is still in draft");
 
         Proposal storage proposal = bounty.proposals[proposalId];
-        require(!isProposalInDraft, "Proposal is still in draft");
-        require(!isProposalDisputed, "Proposal is disputed, cant accept");
+        require(!isProposalInDraft(id, proposalId), "Proposal is still in draft");
+        require(!isProposalDisputed(id, proposalId), "Proposal is disputed, cant accept");
 
         bounty.closed = true;
 
