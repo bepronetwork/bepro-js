c267ca31cc8ef9530009908174fdc06fc5e01cce
moshmage <moshmage@gmail.com>
2022-03-02 01:37:17 +0000

    simplifies post-install.js

--- tooling/post-install.js
+++ tooling/post-install.js
@@ -1,23 +1,27 @@
 const fs = require('fs');
 const path = require('path');
 
-if (fs.existsSync('dist')) {
-  console.log(`bepro-js sdk was already built.`)
-  return 0;
-}
+const DIST_PATH = `dist`;
 
-async function buildSolution() {
+function buildSolution() {
   try {
-    console.log(`Building bepro-js sdk`);
+
     const {execSync} = require("child_process");
-
     const cwd = path.resolve();
+    const wasBuilt = fs.statSync(path.resolve(cwd, DIST_PATH))?.isDirectory();
+    const hasDependencies = fs.statSync(path.resolve(cwd, `node_modules`, `truffle`))?.isDirectory();
 
-    if (!fs.existsSync(path.resolve(cwd, `node_modules`, `truffle`)))
-      await execSync(`npm install .`, {stdio: 'inherit', cwd});
+    if (wasBuilt) {
+      console.log(`bepro-js sdk was already built.`)
+      return 0;
+    }
 
-    if (!fs.existsSync(path.resolve(cwd, `dist`)))
-      await execSync(`npm run build`, {stdio: 'inherit', cwd});
+    console.log(`Building bepro-js sdk`);
+
+    if (!hasDependencies)
+      execSync(`npm install .`, {stdio: 'inherit', cwd});
+
+    execSync(`npm run build`, {stdio: 'inherit', cwd});
 
     console.log(`Built bepro-js sdk`);
     return 0;
@@ -28,4 +32,4 @@ async function buildSolution() {
   }
 }
 
-(async () => await buildSolution())();
+buildSolution();
